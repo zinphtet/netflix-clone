b5acf3af2fd5da0d58e5599c6f3e031647d1676c
Zin PHtet <zinpainghtet.215108@gmail.com>
2022-08-10 22:21:24 +0630

    background image fixed

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,13 +3,15 @@ import style from './Header.module.css'
 import Navbar from '../Navbar/Navbar'
 import Banner from '../banner/Banner'
 import Image from 'next/image'
+import clifford from './clifford.webp'
 
 const Header = () => {
-  // const backStyle = {
-  //   backgroundImage : `url(${Banner})`
-  // }
+  const backStyle = {
+    backgroundImage : `url(${clifford.src})`
+  }
+  console.log("My Image" , clifford)
   return (
-    <div className={style.header} >
+    <div className={style.header} style={backStyle}>
               <Navbar/>
               <Banner/>
               {/* <div className={style.back}>
